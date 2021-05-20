0febd72f8b9d7cb1606d10808013266300c49647
iain801 <CAKEFOXMC@GMAIL.COM>
2021-05-20 08:11:02 -0700

    classifier

---
+++ classifier.py
@@ -0,0 +1,201 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+
+# Download and explore dataset
+import pathlib
+dataset_url = "https://github.com/ashleyyz/ai-project/raw/master/images.tar"
+data_dir = tf.keras.utils.get_file('images', origin=dataset_url, untar=True)
+data_dir = pathlib.Path(data_dir)
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+
+# Create a dataset
+batch_size = 16
+img_height = 200
+img_width = 200
+
+# 80/20 validation split
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+# Print class names
+class_names = train_ds.class_names
+print(class_names)
+
+# Visualize data
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+    
+# Training batches
+for image_batch, labels_batch in train_ds:
+  print(image_batch.shape)
+  print(labels_batch.shape)
+  break
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# Normalize data
+normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixels values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
+
+# Create Model
+num_classes = 3
+
+data_augmentation = keras.Sequential(
+  [
+    layers.experimental.preprocessing.RandomFlip("horizontal", 
+                                                 input_shape=(img_height, 
+                                                              img_width,
+                                                              3)),
+    layers.experimental.preprocessing.RandomRotation(0.1),
+    layers.experimental.preprocessing.RandomZoom(0.1),
+  ]
+)
+
+model = Sequential([
+  data_augmentation,
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+# Compile model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+# Train Model
+epochs = 25
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+# Visualize training results
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+# Save Model
+model.save("pokemon_model")
+del model
+
+model = load_model('pokemon_model')
+
+# Piplup Test
+piplup_url = "https://static.wixstatic.com/media/2e36a5_effd17ea4d0d4c3fafbb6f131a0dd186~mv2.jpg/v1/fill/w_760,h_428,al_c,q_90/2e36a5_effd17ea4d0d4c3fafbb6f131a0dd186~mv2.jpg"
+piplup_path = tf.keras.utils.get_file('2e36a5_effd17ea4d0d4c3fafbb6f131a0dd186~mv2', origin=piplup_url)
+
+img = keras.preprocessing.image.load_img(
+    piplup_path, target_size=(img_height, img_width)
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This Piplup most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# Litwick Test
+litwick_url = "https://i.pinimg.com/originals/71/4c/94/714c942405dd3f487b50e86a30a9e1fe.jpg"
+litwick_path = tf.keras.utils.get_file('714c942405dd3f487b50e86a30a9e1fe', origin=litwick_url)
+
+img = keras.preprocessing.image.load_img(
+    litwick_path, target_size=(img_height, img_width)
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This Blazekin most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# Shaymin Test
+shaymin_url = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/08cb8003-d697-4d4d-9f14-4bbc8ea9c929/d35vwgn-bcc0850e-2c51-4c97-a4ee-3706afff9fa9.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi8wOGNiODAwMy1kNjk3LTRkNGQtOWYxNC00YmJjOGVhOWM5MjkvZDM1dndnbi1iY2MwODUwZS0yYzUxLTRjOTctYTRlZS0zNzA2YWZmZjlmYTkuanBnIn1dXX0.VMxLNS_0E52xJcod3rk3xZcwU5GFDHAagA5hW4WEcHY"
+shaymin_path = tf.keras.utils.get_file('d35vwgn-bcc0850e-2c51-4c97-a4ee-3706afff9fa9', origin=shaymin_url)
+
+img = keras.preprocessing.image.load_img(
+    shaymin_path, target_size=(img_height, img_width)
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This Grass-Type most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+) 
